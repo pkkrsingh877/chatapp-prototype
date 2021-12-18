4334236e6554ef4a2d5b61848ce179ac2119ad1b
Prabhat Kumar <pkkrsingh877@gmail.com>
2021-12-18 06:14:15 +0530

    ejs setup complete also added bootstrap

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const port = process.env.PORT || 3000;
+const methodOverride = require('method-override')
+const morgan = require('morgan');
+const ejs = require('ejs');
+const ejsMate = require('ejs-mate');
+const mongoose = require('mongoose');
+
+//middleware (order matters )
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(methodOverride('_method'));
+app.use(morgan('dev'));
+
+mongoose.connect('mongodb://localhost:27017/chatapp-prototype', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+)
+.then(() => {
+    console.log("DB Connection Successful!")
+})
+.catch((error) => {
+    console.log(error);
+});
+
+//setting up ejs
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.send('Working');
-    // res.render('index');
+    // res.send('Working');
+    res.render('index');
 });
 
 app.listen(3000, () => {
