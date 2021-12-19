26b9d0025c88141e2ddf8a04bbed09aa26cab234
Prabhat Kumar <pkkrsingh877@gmail.com>
2021-12-19 07:55:02 +0530

    sign up and login feature half done

--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/users');
+
+router.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username: username, password: password });
+        if(user){
+            console.log(user);
+            console.log("User logged in!");
+        }else{
+            console.log('Invalid Username or Password!');
+        }
+        res.redirect('/');
+    } catch (err) {
+        console.log(err);
+    }
+});
 
 router.get('/login', (req, res) => {
     res.render('auth/login');
 });
 
+router.post('/signup', async (req, res) => {
+    try {
+        const { name, username, email, password } = req.body;
+        const user = await User.create({
+            name: name,
+            username: username,
+            email: email,
+            password: password
+        });
+        console.log(user);
+        res.redirect('/');
+    } catch (err) {
+        console.log(err);
+    }
+});
+
 router.get('/signup', (req, res) => {
     res.render('auth/signup');
-});
-
-router.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    console.log(req.body);
-    res.end();
-});
-
-router.post('/signup', (req, res) => {
-    const { name, username, email, password } = req.body;
-    console.log(req.body);
-    res.end();
 });
 
 module.exports = router;
